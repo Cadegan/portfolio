9868e595eeb2dee066610e4d81c65965a6a67226
Cadegan <91974649+Cadegan@users.noreply.github.com>
2023-02-02 14:53:45 +0900

    Portfolio

---
+++ src/Header/index.jsx
@@ -0,0 +1,12 @@
+import "./header.css";
+
+export default function Header() {
+  return (
+    <header>
+      <div className="logo">
+        <span className="firstLetter">J</span>
+        <span>ean-charles</span>
+      </div>
+    </header>
+  );
+}
