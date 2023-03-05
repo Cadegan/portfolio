7ca588db9bbccddf6efdd799b4b92874ad14717e
Cadegan <91974649+Cadegan@users.noreply.github.com>
2023-03-05 16:19:25 +0900

    Mobile menu v0

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -1,16 +1,19 @@
 import { BrowserRouter } from "react-router-dom";
 import About from "../components/About";
 import Skills from "../components/Skills";
-import DesktopNav from "../components/Header";
+import DesktopNav from "../components/Navigation";
 import Profile from "../components/home";
 import Projects from "../components/Projects";
+import { Example } from "../components/Navigation/Mobile/Exemple";
 
 function App() {
   return (
     <>
       <BrowserRouter>
         <DesktopNav></DesktopNav>
+
         <main>
+          <Example></Example>
           <Profile></Profile>
           <About></About>
           <Skills></Skills>
